a9e1a90162c2904103614a70c2e2d2983a1a9cf1
Dennys William <dennyswilliam018@gmail.com>
2024-10-03 20:32:37 -0300

    :sparkles: Fix Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.BACKGROUND};
-  height: 180px;
+  height: 200px;
   width: 100%;
   display: flex;
   flex-direction: column;
